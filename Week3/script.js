9a51ed365b104afea4f7e10f0001b88d25b28e16
Jiri Holanek <22113317@ibat.ie>
2021-10-06 19:26:22 +0100

    creating arrays and maps

--- Week3/script.js
+++ Week3/script.js
@@ -12,31 +12,39 @@ let carType = 'Hatchback';
 // let carInterior = 'Luxurious';
 
 let car = {
-    'mf' : 'Ford',
-    'model' : 'fiesta',
-    'engine' : '1.25',
-    'color' : 'pink',
-    'type' : 'sedan',
+    'mf': 'Ford',
+    'model': 'fiesta',
+    'engine': '1.25',
+    'color': 'pink',
+    'type': 'sedan',
 }
 
- let fleet = [
+let fleet = [
     {
-        'mf' : 'Ford',
-        'model' : 'fiesta',
-        'engine' : '1.25',
-        'color' : 'pink',
-        'type' : 'sedan',
+        'mf': 'Ford',
+        'model': 'fiesta',
+        'engine': '1.25',
+        'color': 'pink',
+        'type': 'sedan',
     },
     {
-        'mf' : 'Toyota',
-        'model' : 'fiesta',
-        'engine' : '2.0',
-        'color' : 'Gray',
-        'type' : 'Sports Coupe',
-    
+        'mf': 'Toyota',
+        'model': 'fiesta',
+        'engine': '2.0',
+        'color': 'Gray',
+        'type': 'Sports Coupe',
+
+    },
+    {
+        'mf': 'Ford',
+        'model': 'cougar',
+        'engine': '2.0',
+        'color': 'Gray',
+        'type': 'Sports Coupe',
+
     }
-    
- ] 
+
+]
 
 
 
@@ -60,11 +68,45 @@ for (let i = 0; i < fleet.length; i++) {
     const message = `${i} : ${currentCar.mf} : ${currentCar.color} : ${currentCar.model}`
 
     console.log(message);
-    
+
 }
 
 for (const currentCar of fleet) {
     const message = `for each : ${currentCar.mf} : ${currentCar.color} : ${currentCar.model}`
     console.log(message);
 
+}
+
+GetAllManufacturers(fleet);
+FindCarsByMf(fleet, 'ford')
+
+function FindCarsByMf(fleet, manufacturer) {
+
+    let carsByManufacturer = fleet.filter(car => car.mf.toLowerCase() === manufacturer.toLowerCase())
+    console.log(carsByManufacturer)
+
+}
+
+function GetAllManufacturers(fleet) {
+    let foundItems = [];
+
+    // for (const currentCar of fleet) {
+    //     foundItems.push(currentCar.mf);
+    // }
+
+    fleet.forEach(function (car) {
+        foundItems.push(car.mf);
+
+    })
+
+    let foundMFs = fleet.map(
+        function (car) {
+            return car.mf
+        }
+    )
+
+
+    const foundMFList = fleet.map(car => car.mf)
+
+    console.log(foundMFList.join());
 }
